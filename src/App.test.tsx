b0b9d390a62f4613b1f4cc83e321e748648e1477
7h1b0 <7h1b0@users.noreply.github.com>
2019-02-21 23:48:11 +0100

    test: cover counter logic

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 import App from './App';
 
 describe('<App />', () => {
   afterEach(cleanup);
 
-  it('should render Hello Word', () => {
+  it('should render Hello Word and increment counter', () => {
     const { getByText } = render(<App />);
 
     expect(getByText('Hello World')).toBeDefined();
+    expect(getByText('0')).toBeDefined();
+
+    fireEvent.click(getByText('Click Me'));
+    expect(getByText('1')).toBeDefined();
   });
 });
