70daae43793e0ba086d252ad7d30c4e0161b979b
7h1b0 <7h1b0@users.noreply.github.com>
2019-12-08 17:52:28 +0100

    chore(deps): upgrade dependencies and move to @babel/preset-modules

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  presets: ['@babel/preset-modules', '@babel/preset-typescript'],
   plugins: [
     '@babel/plugin-transform-react-jsx',
     '@babel/plugin-proposal-class-properties',
