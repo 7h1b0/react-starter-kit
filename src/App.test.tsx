93f980669bc1663533e4ae0bce45d270bffd063f
7h1b0 <7h1b0@users.noreply.github.com>
2020-03-15 14:51:24 +0100

    test: add jest-dom and remove cleanup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 import App from './App';
 
 describe('<App />', () => {
-  afterEach(cleanup);
-
   it('should render Hello Word and increment counter', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText('Hello World')).toBeDefined();
-    expect(getByText('0')).toBeDefined();
+    expect(getByText('Hello World')).toBeVisible();
+    expect(getByText('0')).toBeVisible();
 
     fireEvent.click(getByText('Click Me'));
-    expect(getByText('1')).toBeDefined();
+    expect(getByText('1')).toBeVisible();
   });
 });
