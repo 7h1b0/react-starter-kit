c2a17226f5f439ddfaaf9ce5e3abc6ab4c19bd09
7h1b0 <7h1b0@users.noreply.github.com>
2020-11-11 15:07:16 +0100

    test: target current node on test

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,27 @@
-module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
-  plugins: [
-    '@babel/plugin-transform-react-jsx',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-syntax-dynamic-import',
-  ],
+module.exports = (api) => {
+  api.cache.using(() => process.env.NODE_ENV);
+  const isTest = api.env('test');
+
+  if (isTest) {
+    return {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript',
+      ],
+      plugins: [
+        '@babel/plugin-transform-react-jsx',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-syntax-dynamic-import',
+      ],
+    };
+  }
+
+  return {
+    presets: ['@babel/preset-env', '@babel/preset-typescript'],
+    plugins: [
+      '@babel/plugin-transform-react-jsx',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-syntax-dynamic-import',
+    ],
+  };
 };
