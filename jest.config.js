2b3fb2a91c7ab44b2d56a9904aa911a5ef645f3a
7h1b0 <7h1b0@users.noreply.github.com>
2021-05-27 20:01:10 +0200

    chore(deps): upgrade dependencies

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src/'],
+  moduleDirectories: ['node_modules'],
+  modulePaths: ['<rootDir>/src/'],
+  collectCoverageFrom: ['**/*.{tsx,ts}'],
+};
