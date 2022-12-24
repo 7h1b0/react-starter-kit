db3beeb642fb4809f847e63f384af750b287e6a8
7h1b0 <7h1b0@users.noreply.github.com>
2022-12-24 22:23:56 +0100

    chore(deps): migrate to Vite

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  roots: ['<rootDir>/src/'],
-  moduleDirectories: ['node_modules'],
-  modulePaths: ['<rootDir>/src/'],
-  collectCoverageFrom: ['**/*.{tsx,ts}'],
-};
