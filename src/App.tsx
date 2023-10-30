6f5bd0507b4e215371ed40f97bbbfe9819963b42
7h1b0 <7h1b0@users.noreply.github.com>
2023-10-30 23:08:36 +0100

    feat: add unoCSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App: React.FunctionComponent = () => {
   const [counter, setCounter] = useState(0);
 
   return (
-    <div>
+    <div className="flex flex-col">
       <h1>Hello World</h1>
       <p>{counter}</p>
       <button onClick={() => setCounter((state) => state + 1)}>Click Me</button>
